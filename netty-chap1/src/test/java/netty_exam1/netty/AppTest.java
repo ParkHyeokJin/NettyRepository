c89ceb0a674bdbd00c8d7dc23d73b763553eeb17
besthj <parkhjpp@daum.net>
2019-12-24 15:58:07 +0900

    Netty Study Chap1

---
+++ netty-chap1/src/test/java/netty_exam1/netty/AppTest.java
@@ -0,0 +1,20 @@
+package netty_exam1.netty;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
